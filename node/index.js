e0c67db73dcf37f8a0ad4ce31f3fb511400a4fd7
FelipeCararo <felipe.gorges.cararo@gmail.com>
2021-04-28 15:59:19 -0300

    Add list people

--- node/index.js
+++ node/index.js
@@ -18,7 +18,27 @@ connection.query(sql);
 connection.end();
 
 app.get('/', (req,res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>');
+  var con = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'pfa'
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+
+    con.query("SELECT * FROM people", function (err, result, fields) {
+      if (err) throw err;
+
+      let items = [];
+      result.forEach(element => {
+        items.push(element.name);
+      });
+      res.send(`<h1>Full Cycle Rocks!</h1> <br>${items.join(', ')}`);
+    });
+
+  });
 });
 
 app.listen(port, ()=> {
